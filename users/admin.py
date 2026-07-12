db73b5ef3c27c3ac29ab682389de333ec985419f
sakshiM <sakshi.mistary@gmail.com>
2026-07-12 17:31:56 +0530

    Update user models

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from users.models import Student, User, Trainer
+
+
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ['user__first_name', 'user__last_name', "user__username", "user__email"]
+
+@admin.register(Trainer)
+class TrainerAdmin(admin.ModelAdmin):
+    pass
--- users/models.py
+++ users/models.py
@@ -1,7 +1,36 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import Model
+
 
 # Create your models here.
 
 class User(AbstractUser):
     mobile_no = models.CharField(max_length=10, unique=True)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    joining_date = models.DateField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def clean(self):
+        if self.user and Trainer.objects.filter(user=self.user).exists():
+            raise ValidationError("User is already Trainer. Cannot create student for it")
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+
+class Trainer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    joining_date = models.DateField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def clean(self):
+        if self.user and Student.objects.filter(user=self.user).exists():
+            raise ValidationError("User is already Student. Cannot create trainer for it")
+
+    def __str__(self):
+        return self.user.get_full_name()
